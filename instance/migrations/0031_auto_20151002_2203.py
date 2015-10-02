1b751f3141a1fe0817ba327fb456aef9b75450ea
Henrique Romano <chromano@gmail.com>
2015-10-02 19:04:12 -0300

    Remove merge migration

---
+++ instance/migrations/0031_auto_20151002_2203.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('instance', '0030_auto_20150927_1045'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='openedxinstance',
+            name='attempts',
+            field=models.SmallIntegerField(default=3, validators=[django.core.validators.MinValueValidator(1)]),
+        ),
+        migrations.AlterField(
+            model_name='openstackserver',
+            name='status',
+            field=models.CharField(default='new', choices=[('new', 'New - Not yet loaded'), ('started', 'Started - Running but not active yet'), ('active', 'Active - Running but not booted yet'), ('booted', 'Booted - Booted but not ready to be added to the application'), ('provisioned', 'Provisioned - Provisioning is completed'), ('provisioning_failure', 'Provisioning failure - Error during provisioning'), ('rebooting', 'Rebooting - Reboot in progress, to apply changes from provisioning'), ('ready', 'Ready - Rebooted and ready to add to the application'), ('live', 'Live - Is actively used in the application and/or accessed by users'), ('stopping', 'Stopping - Stopping temporarily'), ('stopped', 'Stopped - Stopped temporarily'), ('terminating', 'Terminating - Stopping forever'), ('terminated', 'Terminated - Stopped forever')], max_length=20, db_index=True),
+        ),
+    ]
