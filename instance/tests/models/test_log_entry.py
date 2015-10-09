6dd05f07901110ddd88748d64f06473fa42a0b32
Henrique Romano <chromano@gmail.com>
2015-10-08 21:21:14 -0300

    Make test more readable

--- instance/tests/models/test_log_entry.py
+++ instance/tests/models/test_log_entry.py
@@ -46,56 +46,37 @@ class LogEntryTestCase(TestCase):
         instance = OpenEdXInstanceFactory(sub_domain='my.instance')
         server = OpenStackServerFactory(instance=instance, openstack_id='vm1_id')
 
-        with freeze_time("2015-08-05 18:07:00"):
-            instance.logger.info('Line #1, on instance')
+        lines = [
+            ("2015-08-05 18:07:00", instance.logger.info, 'Line #1, on instance'),
+            ("2015-08-05 18:07:01", server.logger.info, 'Line #2, on server'),
+            ("2015-08-05 18:07:02", instance.logger.debug, 'Line #3, on instance (debug, not published by default)'),
+            ("2015-08-05 18:07:03", instance.logger.info, 'Line #4, on instance'),
+            ("2015-08-05 18:07:04", instance.logger.warn, 'Line #5, on instance (warn)'),
+            ("2015-08-05 18:07:05", server.logger.info, 'Line #6, on server'),
+            ("2015-08-05 18:07:06", server.logger.critical, 'Line #7, exception'),
+        ]
 
-        with freeze_time("2015-08-05 18:07:01"):
-            server.logger.info('Line #2, on server')
+        for date, log, text in lines:
+            with freeze_time(date):
+                log(text)
 
-        with freeze_time("2015-08-05 18:07:02"):
-            instance.logger.debug('Line #3, on instance (debug, not published by default)')
-
-        with freeze_time("2015-08-05 18:07:03"):
-            instance.logger.info('Line #4, on instance')
-
-        with freeze_time("2015-08-05 18:07:04"):
-            instance.logger.warn('Line #5, on instance (warn)')
-
-        with freeze_time("2015-08-05 18:07:05"):
-            server.logger.info('Line #6, on server')
-
-        with freeze_time("2015-08-05 18:07:06"):
-            server.logger.critical('Line #7, exception')
+        instance_prefix = 'instance.models.instance  | instance=my.instance | '
+        server_prefix = 'instance.models.server    | instance=my.instance,server=vm1_id | '
+        expected = [
+            ("2015-08-05 18:07:00", 'INFO', instance_prefix + 'Line #1, on instance'),
+            ("2015-08-05 18:07:01", 'INFO', server_prefix + 'Line #2, on server'),
+            ("2015-08-05 18:07:03", 'INFO', instance_prefix + 'Line #4, on instance'),
+            ("2015-08-05 18:07:04", 'WARNING', instance_prefix + 'Line #5, on instance (warn)'),
+            ("2015-08-05 18:07:05", 'INFO', server_prefix + 'Line #6, on server'),
+            ("2015-08-05 18:07:06", 'CRITICAL', server_prefix + 'Line #7, exception'),
+        ]
 
         entries = instance.log_entries
-        self.assertEqual(entries[0].level, "INFO")
-        self.assertEqual(entries[0].created.strftime("%Y-%m-%d %H:%M:%S"), "2015-08-05 18:07:00")
-        self.assertEqual(entries[0].text,
-                         "instance.models.instance  | instance=my.instance | Line #1, on instance")
 
-        self.assertEqual(entries[1].level, "INFO")
-        self.assertEqual(entries[1].created.strftime("%Y-%m-%d %H:%M:%S"), "2015-08-05 18:07:01")
-        self.assertEqual(entries[1].text,
-                         "instance.models.server    | instance=my.instance,server=vm1_id | Line #2, on server")
-        self.assertEqual(entries[2].level, "INFO")
-        self.assertEqual(entries[2].created.strftime("%Y-%m-%d %H:%M:%S"), "2015-08-05 18:07:03")
-        self.assertEqual(entries[2].text,
-                         "instance.models.instance  | instance=my.instance | Line #4, on instance")
-
-        self.assertEqual(entries[3].level, "WARNING")
-        self.assertEqual(entries[3].created.strftime("%Y-%m-%d %H:%M:%S"), "2015-08-05 18:07:04")
-        self.assertEqual(entries[3].text,
-                         "instance.models.instance  | instance=my.instance | Line #5, on instance (warn)")
-
-        self.assertEqual(entries[4].level, "INFO")
-        self.assertEqual(entries[4].created.strftime("%Y-%m-%d %H:%M:%S"), "2015-08-05 18:07:05")
-        self.assertEqual(entries[4].text,
-                         "instance.models.server    | instance=my.instance,server=vm1_id | Line #6, on server")
-
-        self.assertEqual(entries[5].level, "CRITICAL")
-        self.assertEqual(entries[5].created.strftime("%Y-%m-%d %H:%M:%S"), "2015-08-05 18:07:06")
-        self.assertEqual(entries[5].text,
-                         "instance.models.server    | instance=my.instance,server=vm1_id | Line #7, exception")
+        for entry, (date, level, text) in zip(entries, expected):
+            self.assertEqual(entry.created.strftime("%Y-%m-%d %H:%M:%S"), date)
+            self.assertEqual(entry.level, level)
+            self.assertEqual(entry.text, text)
 
     @patch('instance.logging.publish_data')
     def test_log_publish(self, mock_publish_data): #pylint: disable=no-self-use
